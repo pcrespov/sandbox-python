ed78a234f70f26e190db1d1f5c88eb388f86ac9b
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2020-06-24 17:37:55 +0200

    formatted and adde reqs

--- keep/test_access.py
+++ keep/test_access.py
@@ -1,21 +1,18 @@
 from collections.abc import Mapping
 
+
 def get(dotted_name: str, data: Mapping):
     pass
+
+
 def get_safe(dotted_name: str, data: Mapping):
     pass
 
 
 def test_it():
-    app = {
-        'a': {
-            'b': {
-                'c': 55
-            }
-        }
-    }
+    app = {"a": {"b": {"c": 55}}}
 
-    get(app, "a.b.c") == app['a']['b']['c']
-    get(app, "a") == app['a']
-    
-    get_safe(app, "a.b.f") == app.get('a', {}).get('b', {}).get('f')
+    get(app, "a.b.c") == app["a"]["b"]["c"]
+    get(app, "a") == app["a"]
+
+    get_safe(app, "a.b.f") == app.get("a", {}).get("b", {}).get("f")
--- keep/test_it.py
+++ keep/test_it.py
@@ -4,13 +4,15 @@
 # pylint:disable=unused-argument
 # pylint:disable=redefined-outer-name
 
-import pytest
-
-import sys
 import os
+import shutil
+import sys
+from itertools import chain
 from os.path import join, relpath
 from pathlib import Path
-import shutil
+
+import pytest
+
 
 @pytest.fixture(scope="session")
 def here():
@@ -27,26 +29,24 @@ def specs_dir(here):
 def list_specs(wildcard):
     return list(map(str, specs_dir(here()).rglob(wildcard)))
 
-@pytest.fixture(scope="module",
-                params= list_specs("*.y*ml") + \
-                        list_specs("*.json"))
+
+@pytest.fixture(scope="module", params=list_specs("*.y*ml") + list_specs("*.json"))
 def api_spec_file(request, specs_dir):
     specs_path = request.param
     assert os.path.exists(specs_path)
     return relpath(specs_path, specs_dir)
 
 
-from itertools import chain
-
 @pytest.fixture("module")
 def api_spec_tails(specs_dir):
     """ Returns spec files path relative to specs_dir """
     tails = []
-    for fpath in chain(*[specs_dir.rglob(wildcard) for wildcard in ("*.json", "*.y*ml")]):
+    for fpath in chain(
+        *[specs_dir.rglob(wildcard) for wildcard in ("*.json", "*.y*ml")]
+    ):
         tail = relpath(fpath, specs_dir)
-        tails.append( Path(tail) )
+        tails.append(Path(tail))
     return tails
-
 
     # for basedir, files, dirs in os.walk(specs_dir):
     #     spec_files = [ n for n in files if n.endswith((".yaml", ".yml", ".json")) ]
@@ -61,14 +61,13 @@ def api_spec_tails(specs_dir):
 @pytest.fixture("module")
 def specs_testdir(here, specs_dir, api_spec_tails, tmpdir_factory):
     # Copies all test
-    testdir = Path( tmpdir_factory.mktemp("new-specs-dir") )
+    testdir = Path(tmpdir_factory.mktemp("new-specs-dir"))
     for tail in api_spec_tails:
         dstdir = testdir / tail.parent
         os.makedirs(dstdir, exist_ok=True)
-        shutil.copy(specs_dir/tail, dstdir)
+        shutil.copy(specs_dir / tail, dstdir)
 
     return testdir
-
 
 
 def test_it(specs_testdir, api_spec_file):
