96a22b964e2fdc0277becf68dc335fa45080e3e1
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-09-18 00:17:23 +0200

    cleanup

--- concurrency/spinner.py
+++ concurrency/spinner.py
@@ -1,10 +1,16 @@
-import time
-import threading
+import functools
 import itertools
 import sys
+import threading
+import time
+
+
 class Signal:
     go: bool = True
-BACKSPACE = '\x08'
+
+
+BACKSPACE = "\x08"
+
 
 def spin(msg, signal):
     write, flush = sys.stdout.write, sys.stdout.flush
@@ -13,18 +19,17 @@ def spin(msg, signal):
         write(snapshot)
         flush()
         write(BACKSPACE * len(snapshot))
-        time.sleep(.1)
+        time.sleep(0.1)
         if not signal.go:
             break
-    write(" " * len(snapshot) + BACKSPACE*len(snapshot))
+    write(" " * len(snapshot) + BACKSPACE * len(snapshot))
 
-import functools
 
 def monitor(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         signal = Signal()
-        spinner = threading.Thread(target=spin, args=('bhat are u zinking!', signal))
+        spinner = threading.Thread(target=spin, args=("bhat are u zinking!", signal))
         spinner.start()
         try:
             result = func(*args, **kwargs)
@@ -32,14 +37,15 @@ def monitor(func):
             signal.go = False
             spinner.join()
         return result
-    return wrapper
 
+    return wrapper
 
 
 @monitor
 def slow_function():
     time.sleep(3)
     return True
+
 
 def main():
     res = slow_function()
