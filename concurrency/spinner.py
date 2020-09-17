e6d68617eb01de8778a857281cc5fea1afb7647b
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-09-18 00:14:36 +0200

    concurrency exos from book

---
+++ concurrency/spinner.py
@@ -0,0 +1,49 @@
+import time
+import threading
+import itertools
+import sys
+class Signal:
+    go: bool = True
+BACKSPACE = '\x08'
+
+def spin(msg, signal):
+    write, flush = sys.stdout.write, sys.stdout.flush
+    for char in itertools.cycle("|/-\\"):
+        snapshot = char + " " + msg
+        write(snapshot)
+        flush()
+        write(BACKSPACE * len(snapshot))
+        time.sleep(.1)
+        if not signal.go:
+            break
+    write(" " * len(snapshot) + BACKSPACE*len(snapshot))
+
+import functools
+
+def monitor(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        signal = Signal()
+        spinner = threading.Thread(target=spin, args=('bhat are u zinking!', signal))
+        spinner.start()
+        try:
+            result = func(*args, **kwargs)
+        finally:
+            signal.go = False
+            spinner.join()
+        return result
+    return wrapper
+
+
+
+@monitor
+def slow_function():
+    time.sleep(3)
+    return True
+
+def main():
+    res = slow_function()
+    print("slept", res)
+
+
+main()
