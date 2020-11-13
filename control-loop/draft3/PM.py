de8918a42d8e0bc9b30571629ad1c9ea174c679f
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2020-11-13 14:46:13 +0100

    Added examples from Wed and adapted code

---
+++ control-loop/draft3/PM.py
@@ -0,0 +1,38 @@
+# Sample provided by Mark Arnold with small changes to adapt it
+import asyncio
+import time
+from pathlib import Path
+
+from simcore_sdk import node_ports
+
+
+def PM(u):
+    pass
+
+
+async def main():
+    Nsim = 10
+    init_simtime_u = "[0,np.array([0.0,0.0,0.0,0.0,0.0,0.0])]"
+    init_simtime_ym = "[0,np.array([0.0,0.0])]"
+    s = ""
+    olds = ""
+    simtime, u = eval(init_simtime_u)
+    simtime, ym = eval(init_simtime_ym)
+    while simtime < Nsim:
+        while olds == s:
+            try:
+                input_1 = open(Path("u.dat"))
+                s = input_1.read()
+            except:
+                s = init_simtime_u
+            time.sleep(1)
+        olds = s
+        simtime, u = eval(s)
+        ym = PM(u)
+        p = Path("ym.dat")
+        output1 = open(p, "w")
+        output1.write("[" + str(simtime + 1) + ",np." + repr(ym) + "]")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
