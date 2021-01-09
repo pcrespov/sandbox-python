76179b3a9a090e3ee70218dd30ff92b1994aa375
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-01-09 23:43:24 +0100

    trials

---
+++ aiopg/test_aiopg.py
@@ -0,0 +1,23 @@
+import psycopg2
+import pytest
+
+from aiopg.sa import Engine, create_engine
+
+DSN = "postgresql://{user}:{password}@{host}:{port}/{database}"
+
+
+async def test_engine_when_pg_not_reachable(loop):
+
+    with pytest.raises((psycopg2.OperationalError,)) as excinfo:
+        engine = await create_engine(
+            DSN.format(
+                database="db", user="foo", password="foo", host="localhost", port=123
+            ),
+            application_name=None,
+            minsize=1,
+            maxsize=4,
+        )
+        # FIXME: ONLY in github-actions raises FileNotFoundError while tear-down engine
+        # aiopg.connection executes self._waiter ---> loop.remove_writer() which raises FileNotFoundError
+
+    assert "could not connect to server" in str(excinfo.value)
