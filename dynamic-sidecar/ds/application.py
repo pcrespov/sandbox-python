1ca9bd4c810f034ac18a65386429a0dd09ef5aee
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2021-04-17 06:28:41 +0200

    missing fix

--- dynamic-sidecar/ds/application.py
+++ dynamic-sidecar/ds/application.py
@@ -1,24 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseSettings
 from . import api
-
-# settings.py ----
-class AppSettings(BaseSettings):
-    debug: bool = False
-
-    # etc
-    class Config:
-        case_sensitive = False
-        env_prefix = "DYNAMIC_SIDECAR_"
+from .settings import AppSettings
 
 
 # application.py ---
-
 
 def create_app():
     app = FastAPI(docs_url="/", title="API concept for dynamic-sidecar")
     app.include_router(api.router)
 
     app.state.settings = AppSettings()
-
+    
     return app
