8cfbdb26013243729139d1011c65e54d09841009
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-05-31 19:13:49 +0200

    Squashed commit of the following:
    commit 763490a9cbb5daa87626237f715058af53c3c691
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Mon May 31 19:11:24 2021 +0200
    
        Exercised with sleepers in dask
    
    commit f1c69fe8385654ccc255236d929084a5b7050783
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Mon May 31 19:11:05 2021 +0200
    
        Adding ipykernel for notebooks
    
    commit 08561337ee782420b6e411b0c2bfe6413d433911
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Thu May 20 20:38:32 2021 +0200
    
        wip
    
    commit 523041f1621345717c22bfc2d3b2b15ced0b92da
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Tue May 18 15:31:12 2021 +0200
    
        mocks
    
    commit fefd4d56d4838927d6658adca70a48f93e349d39
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Thu May 6 21:26:46 2021 +0200
    
        minor on dask
    
    commit 51663559af9e1fc7030346e17dcf1acb8d55f3c6
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Thu May 6 17:11:59 2021 +0200
    
        persistence
    
    commit 5f4a88e591b6a30bba9e32a3cd65657439282779
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Thu May 6 16:14:05 2021 +0200
    
        sample
    
    commit 60e3448f709f9e9d818575c68b6aae7cfb2324ee
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Fri Apr 30 21:23:53 2021 +0200
    
        update sleepers example
    
    commit d845da5c279c9c5ffba04c29ad0276c520a90c0a
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Fri Apr 30 11:43:14 2021 +0200
    
        Added simple pipeline
    
    commit 8bfceec35bb623fe1b4720953b7e5f84ae42d9b2
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Fri Apr 30 10:22:42 2021 +0200
    
        reviewed
    
    commit 5479010bb1f27164db2f61e348d550999c3c8e2d
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Fri Apr 30 10:22:37 2021 +0200
    
        tooling
    
    commit 4153dbdbf49f1147161ef85268e0d7ac3146fb8b
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Fri Apr 30 08:55:29 2021 +0200
    
        minor
    
    commit 85718c53bd1b45ce9dad465f65045c591f0eda80
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Mon Apr 26 21:08:58 2021 +0200
    
        getting started
    
    commit 8ca57728e5e7e64151735d12dd8f4c69212a03b1
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Mon Apr 26 19:12:03 2021 +0200
    
        Taken from https://github.com/flavienbwk/prefect-docker-compose
    
    commit 694b8318f489dc8e263d91411769dcb03762b2d7
    Author: Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
    Date:   Mon Apr 26 19:11:49 2021 +0200
    
        rename

---
+++ prefect-tutorial/aircraftlib.py
@@ -0,0 +1,3 @@
+
+
+# Placeholder for https://github.com/PrefectHQ/prefect/tree/master/examples/tutorial/aircraftlib
--- pytest-mocker/helpers.py
+++ pytest-mocker/helpers.py
@@ -3,3 +3,20 @@ import asyncio
 async def coro(n=1):
     await asyncio.sleep(n)
     return 3
+
+    
+
+class Foo:
+    def bar(self):
+        return 3
+
+    def zee(self):
+        return 7
+
+
+
+def some_func_using_Foo():
+    foo = Foo()
+    print("bar", foo.bar())
+    print("zee", foo.zee())
+    return foo.bar()
