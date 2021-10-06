d3c05658ab7d1c2f525a3fed8c03574cd53744f1
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-10-06 11:17:20 +0200

    created branch ONLY for pytest trials

--- compose-settings/modules/mod2_settings.py
+++
@@ -1,6 +0,0 @@
-from settingslib.base_settings import BaseCustomSettings
-
-class AnotherModuleSettings(BaseCustomSettings):
-    """ Settings for module 2 """
-
-    MYMODULE2_SOME_OTHER_VALUE: int
--- fastapi/pagination.py
+++
@@ -1,5 +0,0 @@
-# SEE https://github.com/uriyyo/fastapi-pagination
-
-# Traeffikc https://github.com/ITISFoundation/osparc-simcore/pull/2031/files
-
-:search see https://cloud.google.com/apis/design/custom_methods
