dc31001bfdf6a49fa7e8148676386ae5426af528
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-11-30 15:08:14 +0100

    option setup-show cleanup

--- pytest-overrides/tests/conftest.py
+++ pytest-overrides/tests/conftest.py
@@ -13,3 +13,15 @@ def username():
 @pytest.fixture
 def other_username(username):
     return 'other-' + username
+
+
+@pytest.fixture(scope="session")
+def foo():
+    return {"x": 33}
+
+
+@pytest.fixture(scope="module")
+def using_foo(foo):
+    bar = foo.copy()
+    bar.update(y=3)
+    return bar
