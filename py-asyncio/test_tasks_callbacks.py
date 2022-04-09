7a9dde1caafee141e71d79ded2b2144995d6abf6
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-04-09 15:20:26 +0200

    adds more asyncio tests

--- py-asyncio/test_tasks_callbacks.py
+++ py-asyncio/test_tasks_callbacks.py
@@ -1,4 +1,5 @@
 import asyncio
+import functools
 
 import pytest
 
@@ -16,7 +17,7 @@ async def fails():
     raise RuntimeError("Failed")
 
 
-def long_task_on_done(fut: asyncio.Future):
+def on_done_log_status(fut: asyncio.Future):
     assert fut.done()
 
     try:
@@ -27,38 +28,58 @@ def long_task_on_done(fut: asyncio.Future):
         fut.result()
         # If the Future is done and has an exception set by the set_exception() method, this method raises the exception.
         # If the Future has been cancelled, this method raises a CancelledError exception.
+        print("done with success")
 
-    except RuntimeWarning as err:
+    except RuntimeError as err:
         print("done with errors", err)
+        # NOTICE that even if not re-raised, the exception is raised if task is awaited!!!
     except asyncio.CancelledError as err:
-        print("cancelled", err)
-        raise
+        print("done with cancellation", err)
+        raise # NOTICE that raising or not raising here will not prevent the other on_done callbacks to ru n!!!!
 
 
-async def hello_world():
-    task = asyncio.create_task(foo())
-
-    def got_result(future):
-        assert future is task
-        print(f"got the result! {future.result()}")
-
-    task.add_done_callback(got_result)
-    print(task)
-    await asyncio.sleep(1)
-    print("Hello World!")
-    await asyncio.sleep(1)
-    print(task)
-
-
-async def test_it():
+async def test_failing_task():
     task = asyncio.create_task(fails(), name="fails")
-    task.add_done_callback(long_task_on_done)
+    task.add_done_callback(on_done_log_status)
 
     with pytest.raises(RuntimeError) as exc_info:
+        # NOTICE that even if long_task_on_done does not re-raise , 
+        # the exception is raised if task is awaited!!!
         await task
+
+    assert exc_info.typename == "RuntimeError" 
+    assert exc_info.value.args == ("Failed",)
+    # TODO: learn what is https://docs.python.org/3/library/contextvars.html#contextvars.Context
+
+
+async def test_cancelled_task():
+    task = asyncio.create_task(fails(), name="fails")
+    task.add_done_callback(functools.partial(print, "\n->First on_done callback for future:"))
+    task.add_done_callback(on_done_log_status)
+    task.add_done_callback(functools.partial(print, "\n->Second on_done callback for future:"))
+
+    
+    task.cancel()
+    with pytest.raises(asyncio.CancelledError):
+        await task
+    
+    assert task.cancelled()
 
     # TODO: learn what is https://docs.python.org/3/library/contextvars.html#contextvars.Context
 
 
-def test_basic():
-    asyncio.run(hello_world())
+async def test_scheduled_background_task():
+
+    def _on_done_print_result(future):
+        assert future is task
+        print(f"got the result and is {future.result()}")
+
+    task = asyncio.create_task(foo())
+    task.add_done_callback(_on_done_print_result)
+    print(task)
+
+    await asyncio.sleep(1)
+    print("Hello World!")
+
+    await asyncio.sleep(1)
+    print(task)
