a65378079615d259832fdfec436d8a8f7ed08460
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-05-03 18:54:35 +0200

    improved

--- pydantic-lib/test_settings.py
+++ pydantic-lib/test_settings.py
@@ -1,12 +1,16 @@
+from dataclasses import Field
 from pydantic import BaseSettings, validator, constr
+
+PATTERN = r"^[a-zA-Z0-9.]*(!=|==){1}[a-zA-Z0-9.]*$"
 
 
 class MySettings(BaseSettings):
     SOME_INT: int
     SOME_LIST: list[int]
-    OTHER_LIST: list[
-        constr(strip_whitespace=True, regex=r"^[a-zA-Z0-9.]*(!=|==){1}[a-zA-Z0-9.]*$")
-    ]
+    OTHER_LIST: list[constr(strip_whitespace=True, regex=PATTERN)]
+    YET_ANOTHER_LIST: list[constr(strip_whitespace=True, regex=PATTERN)]
+
+    EMPTY_LIST: list[constr(strip_whitespace=True, regex=PATTERN)] = []
 
     class Config:
         env_file = "pydantic-lib/.env-settings"  # <<<< Check content of this file
@@ -23,3 +27,8 @@ def test_it():
     assert settings.SOME_INT == 42
     assert settings.SOME_LIST == [1, 2, 3, 4, 5]
     assert settings.OTHER_LIST == ["one==yes", "two!=no"]
+
+    assert settings.YET_ANOTHER_LIST == [
+        "one==yes",
+    ]
+    assert settings.EMPTY_LIST == []
