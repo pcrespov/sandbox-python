1ae4462697ac91f4aa52fd677059d0af1a1f480f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-06-21 17:02:09 +0200

    names tasks

--- py-asyncio/naming_tasks.py
+++ py-asyncio/naming_tasks.py
@@ -11,9 +11,10 @@ async def say_after(delay, what):
 
 async def main():
     task1 = asyncio.create_task(say_after(1, "hoi"), name="bar")
-
     task2 = asyncio.create_task(say_after(2, "zaeme"), name="bar")
 
+
+    print( [t.get_name() for t in asyncio.all_tasks()] )
     print(f"started at {time.strftime('%X')}")
 
     # Wait until both tasks are completed (should take
