d390af7a5a87cb1d710e6029bb5fab0433c54211
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2022-06-22 13:51:22 +0200

    state is frozen

--- aiohttp-lib/app_storage.py
+++ aiohttp-lib/app_storage.py
@@ -9,21 +9,35 @@ class A:
     x: int
 
 
+# ROUTES
 async def hi(request):
-    print(app[A.__name__])
+    print(request.app[A.__name__])
     return web.Response(text="hoi zaeme")
 
 
-app = web.Application()
-app.add_routes([web.get("/", hi)])
+# EVENTS
+async def on_startup(app: web.Application):
+    # using the class itself
+    # app[A] = A(x=3)
+    # or
+    app[A.__name__] = A(x=3)
 
 
+async def on_cleanup(app: web.Application):
+    # NOTE: DeprecationWarning: Changing state of started or joined application is deprecated
+    # del app[A.__name__]
+    # app[A.__name__] = None
+    print("app state", dict(app))
 
-# using the class itself 
-# app[A] = A(x=3)
-# or
-app[A.__name__] = A(x=3)
+
+# MAIN
+def create_app():
+    app = web.Application()
+    app.add_routes([web.get("/", hi)])
+    app.on_startup.append(on_startup)
+    app.on_cleanup.append(on_cleanup)
+    return app
 
 
 if __name__ == "__main__":
-    web.run_app(app)
+    web.run_app(create_app())
