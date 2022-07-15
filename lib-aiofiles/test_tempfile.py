0512524bcfa134df732460b17e91cea9d5fe0892
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2022-07-15 02:23:50 +0200

    tempfile

---
+++ lib-aiofiles/test_tempfile.py
@@ -0,0 +1,85 @@
+import asyncio
+import tempfile
+from collections import Counter
+from contextlib import asynccontextmanager, contextmanager
+from pathlib import Path
+from typing import AsyncIterator, Iterator
+
+import aiofiles
+import aiofiles.os
+import aiofiles.tempfile
+import pytest
+
+
+@asynccontextmanager
+async def aio_write_tmp_file(file_contents: str) -> AsyncIterator[Path]:
+    """Disposes of file on exit"""
+    # pylint: disable=protected-access,stop-iteration-return
+    file_path = Path("/") / f"tmp/{next(tempfile._get_candidate_names())}"  # type: ignore
+    async with aiofiles.open(file_path, mode="w") as tmp_file:
+        await tmp_file.write(file_contents)
+    try:
+        yield file_path
+    finally:
+        await aiofiles.os.remove(file_path)
+
+
+def sample(n):
+    with tempfile.TemporaryDirectory(prefix=f"sample_{n}") as tmpdir:
+        fpath = Path(tmpdir) / "docker-compose.yml"
+        assert fpath.parent.exists()
+
+        with open(fpath, "wt") as fh:
+            fh.write("stuff" * 10_000)
+
+        assert fpath.exists()
+
+    assert not fpath.exists()
+    return fpath
+
+
+async def aio_sample(n):
+    async with aiofiles.tempfile.TemporaryDirectory(prefix=f"sample_{n}") as tmpdir:
+        fpath = Path(tmpdir) / "docker-compose.yml"
+        assert fpath.parent.exists()
+
+        async with aiofiles.open(fpath, "wt") as fh:
+            await fh.write("stuff" * 10_000)
+
+        assert fpath.exists()
+
+    assert not fpath.exists()
+    return fpath
+
+
+Ns = (1, 10, 100, 1_000, 10_000)
+
+
+@pytest.mark.parametrize("N", Ns)
+async def test_aiofiles(N):
+    results = await asyncio.gather(
+        *(aio_sample(n) for n in range(N)), return_exceptions=True
+    )
+    assert not any(isinstance(r, Exception) for r in results)
+    assert len(set(results)) == N
+
+
+@pytest.mark.parametrize("N", Ns)
+async def test_sync_files(N):
+    results = [sample(n) for n in range(N)]
+    assert len(set(results)) == N
+
+
+# pytest --durations=0 --asyncio-mode=auto test_tempfile.py
+#
+#
+# 9.54s call     test_tempfile.py::test_aiofiles[10000]
+# 6.42s call     test_tempfile.py::test_sync_files[10000]
+# 0.85s call     test_tempfile.py::test_aiofiles[1000]
+# 0.62s call     test_tempfile.py::test_sync_files[1000]
+# 0.09s call     test_tempfile.py::test_aiofiles[100]
+# 0.07s call     test_tempfile.py::test_sync_files[100]
+# 0.01s call     test_tempfile.py::test_sync_files[10]
+# 0.01s call     test_tempfile.py::test_aiofiles[10]
+# 0.01s call     test_tempfile.py::test_sync_files[1]
+# 0.01s call     test_tempfile.py::test_aiofiles[1]
