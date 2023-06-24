266216f5de0905aa265a31e09ea8c5f37167500f
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-06-24 15:53:02 +0200

    Trials with pagination

--- fastapi-pagination/main.py
+++ fastapi-pagination/main.py
@@ -1,9 +1,14 @@
+from typing import Annotated
+
+import httpx
+import uvicorn
 from faker import Faker
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from fastapi_pagination import add_pagination, paginate
+from fastapi_pagination.limit_offset import LimitOffsetPage, LimitOffsetParams
 
 # Includes HATEOAS https://apiguide.readthedocs.io/en/latest/build_and_publish/hateos.html
-from fastapi_pagination.links import LimitOffsetPage, Page
+from fastapi_pagination.links import Page
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -30,12 +35,21 @@ async def get_users_with_page_number():
 
 
 @app.get("/users2", response_model=LimitOffsetPage[UserOut])
-async def get_user_with_limit_and_offset():
+async def get_users_with_limit_and_offset():
     """Uses limit-offset pagination: link a moving window"""
     return paginate(users)  # use paginate function to paginate your data
+
+
+@app.get("/users3", response_model=LimitOffsetPage[UserOut])
+async def get_users_from_users2(page_params: Annotated[LimitOffsetParams, Depends()]):
+    print(page_params)
+    page = httpx.get("http://127.0.0.1:8000/users2")
+    print(page)
 
 
 add_pagination(app)  # important! add pagination to your app
 
 
-# uvicorn main:app --reload
+if __name__ == "__main__":
+    # uvicorn main:app --reload
+    uvicorn.run(app, host="0.0.0.0", port=8000)
