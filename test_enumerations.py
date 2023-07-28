99bf5f081c0ca1f77c991b6273688719ba02e7a0
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-07-28 17:14:24 +0200

    testing enums

--- test_enumerations.py
+++ test_enumerations.py
@@ -1,76 +1,54 @@
 import enum
 from enum import Enum, auto, unique
+from typing import Any
+
+
+def test_equivalent_enums_are_not_strictly_equal():
+    class Color1(Enum):
+        RED = "RED"
+        BLUE = "BLUE"
+
+    class Color2(Enum):
+        RED = "RED"
+        BLUE = "BLUE"
+
+    assert Color1 != Color2
+
+    def to_dict(enum_cls: type[Enum]) -> dict[str, Any]:
+        return {m.name: m.value for m in enum_cls}
+
+    assert to_dict(Color1) == to_dict(Color2)
 
 
 @unique
-class RunningState(str, Enum):
-    """State of execution of a project's computational workflow
-
-    SEE StateType for task state
-    """
-
-    UNKNOWN = "UNKNOWN"
-    PUBLISHED = "PUBLISHED"
-    NOT_STARTED = "NOT_STARTED"
-    PENDING = "PENDING"
-    STARTED = "STARTED"
-    RETRY = "RETRY"
-    SUCCESS = "SUCCESS"
-    FAILED = "FAILED"
-    ABORTED = "ABORTED"
-
-    @property
-    def is_running(self) -> bool:
-        return self in (
-            RunningState.PUBLISHED,
-            RunningState.PENDING,
-            RunningState.STARTED,
-            RunningState.RETRY,
-        )
-
-
-print(RunningState.UNKNOWN.is_running)
-print(RunningState.PUBLISHED.is_running)
-
-
-class ConfirmationAction(enum.Enum):
+class SomeEnum(enum.Enum):
     REGISTRATION = "REGISTRATION"
-    RESET_PASSWORD = "RESET_PASSWORD"
-    CHANGE_EMAIL = "CHANGE_EMAIL"
     INVITATION = "INVITATION"
 
 
 @unique
-class ConfirmationActionNEW(str, enum.Enum):
-    REGISTRATION = "REGISTRATION_NEW"
-    RESET_PASSWORD = "RESET_PASSWORD"
-    CHANGE_EMAIL = "CHANGE_EMAIL"
+class SomeStrAndEnum(str, enum.Enum):
+    REGISTRATION = "REGISTRATION"
     INVITATION = "INVITATION"
-
-
-class UserStatusNEW(str, Enum):
-    """
-    pending: user registered but not confirmed
-    active: user is confirmed and can use the platform
-    expired: user is not authorized because it expired after a trial period
-    banned: user is not authorized
-    """
-
-    CONFIRMATION_PENDING = "PENDING"
-    ACTIVE = "ACTIVE"
-    EXPIRED = "EXPIRED"
-    BANNED = "BANNED"
 
 
 def test_inherits_from_str_and_enum():
 
-    assert f"{ConfirmationAction.REGISTRATION}" == "ConfirmationAction.REGISTRATION"
-    assert ConfirmationAction.REGISTRATION != "REGISTRATION"
-    assert ConfirmationAction.REGISTRATION.name == "REGISTRATION"
+    # here are the differences
+    assert f"{SomeEnum.REGISTRATION}" == "SomeEnum.REGISTRATION"
+    assert f"{SomeStrAndEnum.REGISTRATION}" == "REGISTRATION"
 
-    assert f"{ConfirmationActionNEW.REGISTRATION}" == "REGISTRATION_NEW"
-    assert ConfirmationActionNEW.REGISTRATION == "REGISTRATION_NEW"
-    assert ConfirmationActionNEW.REGISTRATION.value == "REGISTRATION_NEW"
+    assert SomeEnum.REGISTRATION != "REGISTRATION"
+    assert SomeStrAndEnum.REGISTRATION == "REGISTRATION"
+
+    assert SomeEnum.REGISTRATION != SomeStrAndEnum.REGISTRATION
+
+    # here are the analogies
+    assert SomeEnum.REGISTRATION.name == "REGISTRATION"
+    assert SomeStrAndEnum.REGISTRATION.name == "REGISTRATION"
+
+    assert SomeEnum.REGISTRATION.value == "REGISTRATION"
+    assert SomeStrAndEnum.REGISTRATION.value == "REGISTRATION"
 
 
 class AutoName(str, Enum):
@@ -97,3 +75,34 @@ def test_autoname():
     assert Ordinal.NORTH == "NORTH"
     assert Ordinal.NORTH.value == "NORTH"
     assert Ordinal.NORTH.name == "NORTH"
+
+
+def test_enum_members():
+    @unique
+    class RunningState(str, Enum):
+        """State of execution of a project's computational workflow
+
+        SEE StateType for task state
+        """
+
+        UNKNOWN = "UNKNOWN"
+        PUBLISHED = "PUBLISHED"
+        NOT_STARTED = "NOT_STARTED"
+        PENDING = "PENDING"
+        STARTED = "STARTED"
+        RETRY = "RETRY"
+        SUCCESS = "SUCCESS"
+        FAILED = "FAILED"
+        ABORTED = "ABORTED"
+
+        @property
+        def is_running(self) -> bool:
+            return self in (
+                RunningState.PUBLISHED,
+                RunningState.PENDING,
+                RunningState.STARTED,
+                RunningState.RETRY,
+            )
+
+    assert not RunningState.UNKNOWN.is_running
+    assert RunningState.PUBLISHED.is_running
