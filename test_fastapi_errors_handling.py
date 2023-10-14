ca7f4e7fa84898cf09661410340e4fc0de0d6001
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2023-10-14 18:03:11 +0200

    error handling with fdastapi

--- test_fastapi_errors_handling.py
+++ test_fastapi_errors_handling.py
@@ -1,17 +1,56 @@
-from fastapi import FastAPI
+import http
+from typing import Any
+
+import pytest
+from fastapi import FastAPI, HTTPException, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from fastapi.testclient import TestClient
-import asyncio
+from pydantic import BaseModel, Field
+
+
+class DefaultErrorModel(BaseModel):
+    error: str = Field(..., description="Standarized error name/code")
+    detail: Any | None = Field(None, description="Further details")
+    message: str = Field(..., description="Human readable error message")
+
+
+async def http_exception_as_json_response(
+    request: Request, exc: HTTPException
+) -> JSONResponse:
+    status_code = http.HTTPStatus(exc.status_code)
+    assert (
+        status_code.phrase == exc.detail
+    )  # defined in starlette.exceptions.HTTPException
+
+    error = DefaultErrorModel(
+        error=f"{exc.status_code}",
+        message=status_code.description,
+        detail=exc.detail,
+    )
+    return JSONResponse(jsonable_encoder(error), status_code=exc.status_code)
 
 
 app = FastAPI()
+app.add_exception_handler(HTTPException, http_exception_as_json_response)
 
 
 @app.get("/error")
 async def _fail():
-    await asyncio.wait_for(asyncio.sleep(100), timeout=1)
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT)
 
 
-def test_it():
+@app.get("/code/{code}")
+async def _code(code: int):
+    raise HTTPException(status_code=code)
+
+
+@pytest.mark.parametrize("code", [e.value for e in http.HTTPStatus])
+def test_it(code: int):
     with TestClient(app) as client:
-        r = client.get("/error")
-        print("response", r)
+        response = client.get(f"/code/{code}")
+
+        assert response.status_code == code
+        model = DefaultErrorModel(**response.json())
+
+        assert model.error == f"{code}"
