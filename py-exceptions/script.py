95a7ee6ad970b4a9a709b8072f77c36641228f79
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-01-19 19:59:11 +0100

    more complex example

--- py-exceptions/script.py
+++ py-exceptions/script.py
@@ -1,6 +1,9 @@
 import random
 
 
+def zoo():
+    raise TypeError
+
 def foo(x):
     if x>0:
         raise ValueError
@@ -9,16 +12,23 @@ def bar():
     foo(random.randint(-10,10))
 
 
+def file_stuff(path):
+    return open(path)
+
 def main():
     try:
         bar()
+        zoo()
     except ValueError:
-        ...
+        file_stuff(".")
 
 
-# `python parse_exceptions.py script.py`
+
+# python parse_exceptions.py script.py
 #
+# Function 'zoo' may raise the following exceptions: TypeError
 # Function 'foo' may raise the following exceptions: ValueError
-# Function 'bar' may raise the following exceptions: None
-# Function 'main' may raise the following exceptions: None
-
+# Function 'bar' may raise the following exceptions: ValueError
+# Function 'file_stuff' may raise the following exceptions: FileNotFoundError, PermissionError
+# Function 'main' may raise the following exceptions: FileNotFoundError, TypeError, PermissionError
+#
