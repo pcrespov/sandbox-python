890529543b96118c83ae7165c98a68ef07866812
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-01-23 18:59:08 +0100

    tests sessions with tuples

--- aiolib-sessions/test_session2.py
+++ aiolib-sessions/test_session2.py
@@ -1,10 +1,13 @@
-import time
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
 
 import aiohttp_session
 import pytest
 from aiohttp import web
 from aiohttp.test_utils import TestClient
-from aiohttp_session import get_session
+from aiohttp_session import AbstractStorage, SimpleCookieStorage, get_session
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 _MIN = 60  # secs
@@ -12,57 +15,62 @@ _HOUR = 60 * _MIN  # secs
 _DAY = 24 * _HOUR  # sec
 
 
+@pytest.fixture
+def cookie_name():
+    return "AIOHTTP_SESSION_FOO"
 
-async def get_session_handler(request):
-    session = await get_session(request)
-    data = session["my_session"]
-    return web.json_response(data)
 
-async def create_session_handler(request):
+@pytest.fixture(params=["secure", "simple"])
+def storage(request: pytest.FixtureRequest, cookie_name: str) -> AbstractStorage:
+
+    if request.param == "secure":
+        return EncryptedCookieStorage(
+            secret_key=b"Thirty  two  length  bytes  key.",
+            cookie_name=cookie_name,
+            max_age=5 * _DAY,
+            # secure=True, WARNING:this is only for HTTPS! the TestClient deletes the cookie on every call with this option
+        )
+    elif request.param == "simple":
+        return SimpleCookieStorage(cookie_name=cookie_name)
+
+
+async def _get_session_handler(request):
     session = await get_session(request)
-    session["my_session"] = ("username", "productname")
+    user_name, product_name = session["my_session"]
+    return web.json_response([user_name, product_name])
+
+
+async def _create_session_handler(request):
+    session = await get_session(request)
+    session["my_session"] = ["my-user-name", "my-product-name"]
 
     return web.Response(text="OK")
 
 
-
-def init():
-    app = web.Application()
-    aiohttp_session.setup(
-        app,
-        EncryptedCookieStorage(
-            secret_key=b"Thirty  two  length  bytes  key.",
-            cookie_name="sandbox_python.aiolib-session.test_session",
-            # max_age=1 * _DAY,
-            # secure=True, WARNING: the TestClient deletes the cookie on every call with this option
-        ),
-    )
-    app.router.add_route("GET", "/session", get_session_handler)
-    app.router.add_route("POST", "/session", create_session_handler)
-    
-    return app
-
-
-# --------------------------------------------------------------------------------------------------------------
-
-
 @pytest.fixture
-def client(event_loop, aiohttp_client) -> TestClient:
-    app = init()
+def client(
+    event_loop, aiohttp_client, storage: AbstractStorage, cookie_name: str
+) -> TestClient:
+
+    # app
+    app = web.Application()
+    app.router.add_route("GET", "/session", _get_session_handler)
+    app.router.add_route("POST", "/session", _create_session_handler)
+    aiohttp_session.setup(app, storage)
+
     return event_loop.run_until_complete(aiohttp_client(app))
 
 
-async def test_it(client: TestClient):
+async def test_it(client: TestClient, cookie_name: str):
 
     response = await client.post("/session")
     assert response.ok
 
+    print(response.cookies[cookie_name].value)
+    # >> response.cookies["AIOHTTP_SESSION"].value
+    #   '{"created": 1706031544, "session": {"my_session": ["username", "productname"]}}'
+
     response = await client.get("/session")
     assert response.ok
     got = await response.json()
-    assert got == ["username", "productname"]
-        
-
-
-# if __name__ == "__main__":
-#     web.run_app(init())
+    assert got == ["my-user-name", "my-product-name"]
