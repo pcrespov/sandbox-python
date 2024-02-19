bd44a4a9f0b3b0891213e98d6f6e0a19229c63f1
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-02-19 14:59:16 +0100

    draft sio example

--- py-socketio/server.py
+++ py-socketio/server.py
@@ -1,13 +1,21 @@
 import socketio
 from aiohttp import web
 
+routes = web.RouteTableDef()
+
+
+@routes.get("/home")
+async def home(request: web.Request):
+    assert request
+    return web.Response(text="Home")
+
 
 def create_app():
     # SEE https://python-socketio.readthedocs.io/en/latest/server.html#aiohttp
     sio = socketio.AsyncServer(
         async_mode="aiohttp",
         cors_allowed_origins=[
-            "http://localhost:5000",
+            "http://0.0.0.0:8080",
             "https://admin.socket.io",
         ],
     )
@@ -19,15 +27,12 @@ def create_app():
     )
 
     app = web.Application()
-
-    @app.get("/home")
-    async def home(request: web.Request):
-        assert request
-        return web.Response(text="Home")
-
-    sio.attach(app)
+    app.router.add_routes(routes)
+    sio.attach(app, socketio_path="socket.io")
     return app
 
 
 if __name__ == "__main__":
+    print("Open https://admin.socket.io")
+    print("user=admin, password=adminadmin")
     web.run_app(create_app())
