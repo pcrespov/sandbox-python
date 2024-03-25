bc7071393f8a9a0dba64466a8f8bc19b4a1c2ae1
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-03-25 19:34:27 +0100

    exceptions

--- test_http_exceptions.py
+++ test_http_exceptions.py
@@ -1,5 +1,6 @@
 import asyncio
 import inspect
+import json
 from http import HTTPStatus
 from typing import Callable
 
@@ -7,6 +8,8 @@ import aiohttp.web_exceptions
 import pytest
 from aiohttp import web
 from aiohttp.test_utils import TestClient
+from pydantic import BaseModel, SecretStr, ValidationError, conint
+from pydantic.json import pydantic_encoder
 
 
 def is_http_exception_with_status(obj):
@@ -140,3 +143,148 @@ async def test_raise_http_responses(client: TestClient, path: str, code: int):
     else:
         assert http_exception_cls_map[code].empty_body
         assert not text
+
+
+class ABody(BaseModel):
+    x: int
+    y: str
+    z: SecretStr
+
+
+class AQuery(BaseModel):
+    count: int = -1
+
+
+class AParam(BaseModel):
+    p: conint(gt=3)
+
+
+@pytest.fixture
+def client2(
+    event_loop: asyncio.AbstractEventLoop,
+    aiohttp_client: Callable,
+    unused_tcp_port_factory: Callable,
+):
+    async def _hi(request: web.Request):
+        # NOTE: this is fail-slow
+        request_errors = []
+        try:
+            p = AParam(**dict(request.match_info))
+        except ValidationError as err:
+            errors = err.errors()
+            for e in errors:
+                e["loc"] = ("path",) + e["loc"]
+            request_errors += errors
+
+        try:
+            q = AQuery(**request.query)
+        except ValidationError as err:
+            errors = err.errors()
+            for e in errors:
+                e["loc"] = ("query",) + e["loc"]
+            request_errors += errors
+
+        try:
+            b = ABody.parse_raw(await request.text())
+        except ValidationError as err:
+            # validation error in body
+            errors = err.errors()
+            for e in errors:
+                e["loc"] = ("body",) + e["loc"]
+            request_errors += errors
+
+        if request_errors:
+            # TODO: strip ctx optionally?
+            envelope = {
+                "error": (
+                    request_errors[0] if len(request_errors) == 1 else request_errors
+                )
+            }
+            raise web.HTTPUnprocessableEntity(
+                reason="Invalid request",
+                text=json.dumps(envelope, default=pydantic_encoder),
+                content_type="application/json",
+            )
+
+    app = web.Application()
+    app.add_routes(
+        [
+            web.get("/hi/{p}", _hi),
+        ]
+    )
+
+    return event_loop.run_until_complete(
+        aiohttp_client(app, server_kwargs={"port": unused_tcp_port_factory()})
+    )
+
+
+async def test_request_error_handler(client2: TestClient):
+    resp = await client2.get("/hi/1", json={"x": "foo"}, params={"count": "wrong"})
+    assert resp.status == web.HTTPUnprocessableEntity.status_code
+    print(resp.reason)
+    print(resp.content_type)
+
+    text = await resp.text()
+    print(text)
+    body = await resp.json()
+    assert body == {
+        "error": [
+            {
+                "loc": ["path", "p"],
+                "msg": "ensure this value is greater than 3",
+                "type": "value_error.number.not_gt",
+                "ctx": {"limit_value": 3},
+            },
+            {
+                "loc": ["query", "count"],
+                "msg": "value is not a valid integer",
+                "type": "type_error.integer",
+            },
+            {
+                "loc": ["body", "x"],
+                "msg": "value is not a valid integer",
+                "type": "type_error.integer",
+            },
+            {
+                "loc": ["body", "y"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+            {
+                "loc": ["body", "z"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+        ]
+    }
+
+
+def test_std_exceptions():
+
+    err = Exception("foo", "bar")
+
+    assert err.args == ("foo", "bar")
+    # str representation
+    assert str(err)
+
+
+def test_validation_error():
+    with pytest.raises(ValidationError) as err_info:
+        ABody(x="foo")
+
+    exc = err_info.value
+    assert isinstance(exc, ValidationError)
+
+    assert exc.errors()
+    print(exc.json(indent=0))
+
+    # str representation
+    assert str(exc)
+
+
+def test_aiohttp_exceptions():
+    exc = _get_http_response(request=None, code=200)
+    assert exc.content_type == "text/plain"
+    assert exc.status == 200
+    assert exc.reason == HTTPStatus(200).phrase
+    assert str(exc) == exc.reason
