f650aad57aced304aac9d0ad56c00723d2274ad0
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-05-22 10:35:41 +0200

    enable/disable mocking

--- respx-lib/test_disable_mock.py
+++ respx-lib/test_disable_mock.py
@@ -13,33 +13,38 @@ from respx import MockRouter
 
 
 @pytest.fixture
-def mocked_service_api(faker: Faker) -> Iterator[MockRouter]:
+def mocked_service_api(faker: Faker, use_real_api: bool) -> Iterator[MockRouter]:
 
     with respx.mock(  # type: ignore
-        base_url="http://director-v2:8000/v2",
+        base_url="https://httpbin.org/",
         assert_all_called=False,
         assert_all_mocked=True,
     ) as respx_mock:
 
-        respx_mock.get(path="/get", name="get").respond(status_code=204)
+        respx_mock.get(path="/get", name="get").respond(status_code=204, json={"x": 42})
 
+        # SEE https://github.com/lundberg/respx/issues/95#issuecomment-721061582
+        if use_real_api:
+            respx_mock.stop()
         yield respx_mock
 
 
-def test_it(mocked_service_api: MockRouter):
-    with httpx.Client(base_url="http://director-v2:8000/v2") as client:
+@pytest.mark.parametrize("use_real_api", [False])
+def test_against_mocked_api(mocked_service_api: MockRouter):
+    with httpx.Client(base_url="https://httpbin.org/") as client:
         resp = client.get("/get")
 
         assert resp.status_code == 204
-
+        assert resp.json() == {"x": 42}
         assert mocked_service_api["get"].called
 
 
-def test_it2(mocked_service_api: MockRouter):
-    mocked_service_api.route(pass_through=True)
+@pytest.mark.parametrize("use_real_api", [True])
+def test_against_real_api(mocked_service_api: MockRouter):
 
     with httpx.Client(base_url="https://httpbin.org/") as client:
         resp = client.get("/get")
 
         assert resp.status_code == 200
-        assert not mocked_service_api["get"].called
+        assert resp.json() != {"x": 42}
+        assert "get" not in mocked_service_api
