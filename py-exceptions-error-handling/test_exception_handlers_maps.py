13cd4d9a10eb193075ecde31655f2e66735fe9ba
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-10-30 16:14:25 +0100

    implemeting it as part of a handler

--- py-exceptions-error-handling/test_exception_handlers_maps.py
+++ py-exceptions-error-handling/test_exception_handlers_maps.py
@@ -3,9 +3,10 @@ from typing import Callable
 
 
 @contextmanager
-def exception_context(
+def handled_exception_context(
     exception_type: type[BaseException] | tuple[type[BaseException], ...],
     exception_handler: Callable,
+    **forward_ctx,
 ):
     try:
         yield
@@ -16,25 +17,47 @@ def exception_context(
         #   - None
         # - must not raise!
         #
-        if exc := exception_handler(e):
+        if exc := exception_handler(e, **forward_ctx):
             assert isinstance(exc, BaseException)
             raise exc from e
 
 
+# handler systems
+
+
+def print_and_ignore_exception(exc):
+    print(f"Caught an exception of type {type(exc).__name__}: {exc}")
+    return None
+
+
+def print_and_reraise_exception(exc):
+    print(f"Reraising {type(exc).__name__}: {exc}")
+    return exc
+
+
+def rest_handler(request):
+    ...
+
+
+def middleware(request):
+    with handled_exception_context(
+        ValueError, print_and_ignore_exception, request=request
+    ):
+        response = rest_handler(request)
+        return response
+
+
 def test_it():
-    def print_and_ignore_exception(exc):
-        print(f"Caught an exception of type {type(exc).__name__}: {exc}")
-        return None
 
-    def print_and_reraise_exception(exc):
-        print(f"Reraising {type(exc).__name__}: {exc}")
-        return exc
-
-    with exception_context((ZeroDivisionError, ValueError), print_and_ignore_exception):
+    with handled_exception_context(
+        (ZeroDivisionError, ValueError), print_and_ignore_exception
+    ):
         raise ZeroDivisionError("this should be handled")
 
-    with exception_context((ZeroDivisionError, ValueError), print_and_ignore_exception):
-        with exception_context(
+    with handled_exception_context(
+        (ZeroDivisionError, ValueError), print_and_ignore_exception
+    ):
+        with handled_exception_context(
             (ZeroDivisionError, ValueError), print_and_reraise_exception
         ):
             raise ZeroDivisionError("this should be  handled at the top")
