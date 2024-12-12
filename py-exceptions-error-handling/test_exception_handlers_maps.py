5ab1099be984cd1eeb7bb8e9adc32a9bf8bf84c2
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2024-12-12 16:42:49 +0100

    eh

--- py-exceptions-error-handling/test_exception_handlers_maps.py
+++ py-exceptions-error-handling/test_exception_handlers_maps.py
@@ -1,6 +1,27 @@
+# pylint: disable=redefined-outer-name
+# pylint: disable=unused-argument
+# pylint: disable=unused-variable
+# pylint: disable=too-many-arguments
+
 import contextlib
 from contextlib import contextmanager
 from typing import Callable
+
+import pytest
+
+
+@contextmanager
+def _handle_by_mapping(
+    catch_exc: type[Exception] | tuple[type[Exception]],
+    reraise_exc: type[Exception],
+    msg_template: str,
+):
+    # CONCEPT!
+    try:
+        yield
+    except catch_exc as exc:
+        msg = msg_template.format_map(exc.__dict__)
+        raise reraise_exc(msg) from exc
 
 
 @contextmanager
@@ -34,6 +55,60 @@ def print_and_ignore_exception(exc):
 def print_and_reraise_exception(exc):
     print(f"Reraising {type(exc).__name__}: {exc}")
     return exc
+
+
+def test_exception_context():
+    # SEE https://docs.python.org/3/library/exceptions.html#exception-context
+
+    def _raise():
+        raise ValueError("Error from _foo")
+
+    with pytest.raises(ValueError) as err_info:
+        _raise()
+
+    exc1 = err_info.value
+    # these object provide information about the context in which the exception was raised:
+    assert exc1.__cause__ is None
+    assert exc1.__context__ is None
+    assert exc1.__suppress_context__ is False
+
+    def _chain_and_reraise():
+        try:
+            _raise()
+        except Exception as exc:
+            msg = "Error from _bar"
+            # explicitly chained exception
+            raise RuntimeError(msg) from exc
+
+    with pytest.raises(RuntimeError) as err_info:
+        _chain_and_reraise()
+
+    exc2 = err_info.value
+    # The expression following from must be an exception or None. It will be set as __cause__ on the raised exception
+    assert isinstance(exc2.__cause__, ValueError)
+    assert (
+        exc2.__suppress_context__ is True
+    )  # Setting __cause__ also implicitly sets the __suppress_context__ attribute to True
+    assert exc2.__context__ == exc2.__cause__
+
+    # Using raise new_exc from None effectively replaces the old exception with the new one for display purposes
+    # (e.g. converting KeyError to AttributeError),
+    #  while leaving the old exception available in __context__ for introspection when debugging.
+    def _replace_and_reraise():
+        try:
+            _raise()
+        except Exception:
+            msg = "Error from _bar"
+            raise RuntimeError(msg) from None
+
+    with pytest.raises(RuntimeError) as err_info:
+        _replace_and_reraise()
+
+    exc3 = err_info.value
+    # The expression following from must be an exception or None. It will be set as __cause__ on the raised exception
+    assert isinstance(exc3.__cause__, type(None))
+    assert isinstance(exc3.__context__, ValueError)
+    # assert exc3.__suppress_context__ is True # NOT TOTALLY CLEAR! ?????
 
 
 def test_basics():
