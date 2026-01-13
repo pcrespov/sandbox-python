d83d3d1fc6b49bf98c675e54c7929969a1bb4e7c
Pedro Crespo-Valero <32402063+pcrespov@users.noreply.github.com>
2026-01-13 14:15:41 +0100

    testing aiohttp example using pytest-asyncio

---
+++ pytest-asyncio-tests/test_aiohttp_with_pytest_asyncio.py
@@ -0,0 +1,36 @@
+"""
+    uv run --with aiohttp --with pytest-asyncio pytest test_aiohttp_with_pytest_asyncio.py
+
+    OR
+
+    uv venv
+    source .venv/bin/activate
+    uv pip install -r requirements-dev.txt
+    pytest test_aiohttp_with_pytest_asyncio.py
+"""
+import pytest
+from aiohttp import web
+from aiohttp.test_utils import TestClient
+
+pytest_plugins = [
+    "aiohttp.pytest_plugin",  # No need to install pytest-aiohttp separately
+]
+
+
+# Example taken from https://docs.aiohttp.org/en/stable/testing.html
+async def hello(request):
+    return web.Response(text="Hello, world")
+
+
+@pytest.fixture
+async def client(aiohttp_client):
+    app = web.Application()
+    app.router.add_get("/", hello)
+    return await aiohttp_client(app)
+
+
+async def test_hello(client: TestClient):
+    resp = await client.get("/")
+    assert resp.status == 200
+    text = await resp.text()
+    assert "Hello, world" in text
